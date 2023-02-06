4f6d5db59e1d15ea1de141783f49da77ec0fd178
karanvirsb <karanvirboparai123@gmail.com>
2023-02-06 09:37:49 -0500

    removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+export default function Home(): JSX.Element {
 	return (
 		<>
 			<div className="text-red-300 text-lg">hi</div>
