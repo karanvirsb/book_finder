b0a10debb2a1a8488beebabf606b4eff72fd9232
karanvirsb <karanvirboparai123@gmail.com>
2023-02-08 16:57:36 -0500

    created test query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import { gql } from "@urql/core";
+import { useEffect, useState } from "react";
+import client from "../urql";
+const QUERY = gql`
+	query Test {
+		getHelloWorld
+	}
+`;
+
+interface testData {
+	getHelloWorld: "Hello World";
+}
 export default function Home(): JSX.Element {
+	const [data, setData] = useState("");
+
+	useEffect(() => {
+		function fetchData(): void {
+			client
+				.query(QUERY, {})
+				.toPromise()
+				.then((data) => {
+					const resp: testData = data.data;
+					setData(resp.getHelloWorld);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+
+		fetchData();
+	}, []);
+
 	return (
 		<>
-			<div className="text-red-300 text-lg">hi</div>
+			<div className="text-red-300 text-lg">{data}</div>
 		</>
 	);
 }
