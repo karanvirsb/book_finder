764e84808e13f47d6a78fbd276cf9597c39c04aa
karanvirsb <karanvirboparai123@gmail.com>
2023-02-14 14:44:07 -0500

    added prisma client along with filling in database

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,4 +1,25 @@
-import { PrismaClient } from "@prisma/client";
+import { type Prisma, PrismaClient } from "@prisma/client";
+import authors from "../model/authors.json" assert { type: "json" };
+import books from "../model/books.json";
+import publishers from "../model/publisher.json";
 const prisma = new PrismaClient();
 
 export default prisma;
+
+async function main(): Promise<void> {
+	console.log(await addAuthors());
+}
+
+main()
+	.catch((e) => {
+		console.error(e.message);
+	})
+	// eslint-disable-next-line @typescript-eslint/no-misused-promises
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
+
+async function addAuthors(): Promise<Prisma.BatchPayload> {
+	const createdAuthors = await prisma.author.createMany({ data: authors });
+	return createdAuthors;
+}
