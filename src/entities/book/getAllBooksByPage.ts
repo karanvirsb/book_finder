acfac77325d2777208af5c2617a71f5b0d7fd0c7
karanvirsb <karanvirboparai123@gmail.com>
2023-02-17 16:36:44 -0500

    added validaiton within a use case

--- src/entities/book/getAllBooksByPage.ts
+++ src/entities/book/getAllBooksByPage.ts
@@ -1,12 +1,41 @@
 import { type PrismaClient, type Books, type Author } from "@prisma/client";
+import { ZodError, z } from "zod";
+
+const getAllBooksByPageSchema = z.object({
+	limit: z.number(),
+	page: z.number(),
+});
+
+export type getAllBooksByPage = z.infer<typeof getAllBooksByPageSchema>;
+
+export interface getAllBooksByPageUCDependencies {
+	getAllBooksByPageDb: ({ limit, page }: getAllBooksByPage) => Promise<
+		| Array<
+				Books & {
+					author: Author;
+				}
+		  >
+		| undefined
+	>;
+}
+
+export function getAllBooksByPageUC({
+	getAllBooksByPageDb,
+}: getAllBooksByPageUCDependencies) {
+	return async function ({ limit, page }: getAllBooksByPage) {
+		try {
+			await getAllBooksByPageSchema.parseAsync({ limit, page });
+			return await getAllBooksByPageDb({ limit, page });
+		} catch (error) {
+			if (error instanceof ZodError) {
+				return error.format();
+			}
+		}
+	};
+}
 
 export interface getAllBooksByPageDependencies {
 	db: PrismaClient;
-}
-
-export interface getAllBooksByPage {
-	limit: number;
-	page: number;
 }
 
 export function getAllBooksByPageDb({ db }: getAllBooksByPageDependencies) {
