c48d661ef1233fc02a97250718d7d74fddda181c
karanvirsb <karanvirboparai123@gmail.com>
2023-02-17 14:27:10 -0500

    added resolver

--- src/schemas/books/resolvers/index.ts
+++ src/schemas/books/resolvers/index.ts
@@ -1,3 +1,8 @@
-const bookResolvers = {};
+import { type ThunkObjMap, type GraphQLFieldConfig } from "graphql";
+import getAllBooksByPage from "./getAllBooksByPage.resolver";
+
+const bookResolvers: ThunkObjMap<GraphQLFieldConfig<any, any, any>> = {
+	getAllBooksByPage,
+};
 
 export default bookResolvers;
