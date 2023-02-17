e0dc6f1d7c9f56cad7f8d8e2b0d8ac32c00ff8a2
karanvirsb <karanvirboparai123@gmail.com>
2023-02-17 14:25:16 -0500

    created type for args

--- src/schemas/books/resolvers/getAllBooksByPage.resolver.ts
+++ src/schemas/books/resolvers/getAllBooksByPage.resolver.ts
@@ -1,12 +1,17 @@
-import { GraphQLList, GraphQLInt } from "graphql";
+import { GraphQLList, GraphQLInt, type GraphQLScalarType } from "graphql";
 import prisma from "../../../prisma";
 import BookType from "../typedef/book-typedef";
 import { type resolver } from "../../types/resolvers";
 
+interface args {
+	limit: number;
+	page: number;
+}
+
 const getAllBooksByPage: resolver = {
 	type: new GraphQLList(BookType),
 	args: { limit: { type: GraphQLInt }, page: { type: GraphQLInt } },
-	async resolve(_, args) {
+	async resolve(_, args: args) {
 		return await prisma.books.findMany({
 			take: args.limit,
 			skip: args.page * args.limit,
