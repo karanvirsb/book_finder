b2c03716067245da99e8f58e7db8d9a2b8fcfe57
karanvirsb <karanvirboparai123@gmail.com>
2023-02-17 16:06:15 -0500

    created use case

---
+++ src/entities/book/getBooksByTitle.ts
@@ -0,0 +1,41 @@
+import { type Books, type Author, type PrismaClient } from "@prisma/client";
+
+interface getBooksByTitleDbDependency {
+	db: PrismaClient;
+}
+
+interface getBooksByTitle {
+	limit: number;
+	page: number;
+	searchQuery: string;
+}
+
+export function getBooksByTitleDb({ db }: getBooksByTitleDbDependency) {
+	return async function ({
+		limit,
+		page,
+		searchQuery,
+	}: getBooksByTitle): Promise<
+		| Array<
+				Books & {
+					author: Author;
+				}
+		  >
+		| undefined
+	> {
+		try {
+			const result = await db.books.findMany({
+				skip: page * limit,
+				take: limit,
+				include: {
+					author: true,
+				},
+				where: { title: { contains: searchQuery } },
+			});
+
+			return result;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+}
