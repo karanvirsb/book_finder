2be0878d8cc09740fa1f19a25ce3b2ee55abca6a
karanvirsb <karanvirboparai123@gmail.com>
2023-02-19 13:52:18 -0500

    exported db and updated name

--- src/entities/book/index.ts
+++ src/entities/book/index.ts
@@ -2,16 +2,16 @@ import prisma from "../../prisma";
 import { getAllBooksByPageDb, getAllBooksByPageUC } from "./getAllBooksByPage";
 import { getBooksByTitleDb, getBooksByTitleUC } from "./getBooksByTitle";
 
-const getAllBooksByPageDbDependency = getAllBooksByPageDb({
+export const getAllBooksByPageDbAccess = getAllBooksByPageDb({
 	db: prisma,
 });
 
 export const getAllBooksByPageUseCase = getAllBooksByPageUC({
-	getAllBooksByPageDb: getAllBooksByPageDbDependency,
+	getAllBooksByPageDb: getAllBooksByPageDbAccess,
 });
 
 // get books by title
-const getBooksByTitleDBAccess = getBooksByTitleDb({ db: prisma });
+export const getBooksByTitleDBAccess = getBooksByTitleDb({ db: prisma });
 
 export const getBooksByTitleUseCase = getBooksByTitleUC({
 	getBooksByTitleDB: getBooksByTitleDBAccess,
