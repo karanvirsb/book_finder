dbb7246c2b642e9046687b2a3a7341769e26220e
karanvirsb <karanvirboparai123@gmail.com>
2023-02-22 11:30:05 -0500

    generated with urql

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -1,4 +1,5 @@
 import gql from "graphql-tag";
+import * as Urql from "urql";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends Record<string, unknown>> = { [K in keyof T]: T[K] };
@@ -8,6 +9,7 @@ export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
 	[SubKey in K]: Maybe<T[SubKey]>;
 };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export interface Scalars {
 	ID: string;
@@ -70,4 +72,102 @@ export interface Publisher {
 	id?: Maybe<Scalars["Int"]>;
 	name?: Maybe<Scalars["String"]>;
 	phone?: Maybe<Scalars["String"]>;
+}
+
+export type GetAllBooksQueryVariables = Exact<{
+	limit?: InputMaybe<Scalars["Int"]>;
+	page?: InputMaybe<Scalars["Int"]>;
+}>;
+
+export interface GetAllBooksQuery {
+	__typename?: "RootQueryType";
+	getAllBooksByPage?: Array<{
+		__typename?: "book";
+		asin?: string | null;
+		description?: string | null;
+		image_url?: string | null;
+		rating?: string | null;
+		title?: string | null;
+		author?: {
+			__typename?: "author";
+			name?: string | null;
+			id?: number | null;
+		} | null;
+	} | null> | null;
+}
+
+export type GetBooksByTitleQueryVariables = Exact<{
+	limit?: InputMaybe<Scalars["Int"]>;
+	page?: InputMaybe<Scalars["Int"]>;
+	searchQuery?: InputMaybe<Scalars["String"]>;
+}>;
+
+export interface GetBooksByTitleQuery {
+	__typename?: "RootQueryType";
+	getBooksByTitleResolver?: Array<{
+		__typename?: "book";
+		asin?: string | null;
+		description?: string | null;
+		image_url?: string | null;
+		rating?: string | null;
+		title?: string | null;
+		author?: {
+			__typename?: "author";
+			name?: string | null;
+			id?: number | null;
+		} | null;
+	} | null> | null;
+}
+
+export const GetAllBooksDocument = gql`
+	query getAllBooks($limit: Int, $page: Int) {
+		getAllBooksByPage(limit: $limit, page: $page) {
+			asin
+			author {
+				name
+				id
+			}
+			description
+			image_url
+			rating
+			title
+		}
+	}
+`;
+
+export function useGetAllBooksQuery(
+	options?: Omit<Urql.UseQueryArgs<GetAllBooksQueryVariables>, "query">
+) {
+	return Urql.useQuery<GetAllBooksQuery, GetAllBooksQueryVariables>({
+		query: GetAllBooksDocument,
+		...options,
+	});
+}
+export const GetBooksByTitleDocument = gql`
+	query getBooksByTitle($limit: Int, $page: Int, $searchQuery: String) {
+		getBooksByTitleResolver(
+			limit: $limit
+			page: $page
+			searchQuery: $searchQuery
+		) {
+			asin
+			author {
+				name
+				id
+			}
+			description
+			image_url
+			rating
+			title
+		}
+	}
+`;
+
+export function useGetBooksByTitleQuery(
+	options?: Omit<Urql.UseQueryArgs<GetBooksByTitleQueryVariables>, "query">
+) {
+	return Urql.useQuery<GetBooksByTitleQuery, GetBooksByTitleQueryVariables>({
+		query: GetBooksByTitleDocument,
+		...options,
+	});
 }
