e3d748e3180c70aa3b89f9702956ef42f92c64e5
karanvirsb <karanvirboparai123@gmail.com>
2023-02-22 12:11:29 -0500

    removed urql  provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import "../styles/globals.css";
-import { Provider } from "urql";
-import client from "../urql";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<Provider value={client}>
-			<Component {...pageProps} />;
-		</Provider>
-	);
+	return <Component {...pageProps} />;
 }
