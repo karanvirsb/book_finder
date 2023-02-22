1f65fc0ad3a422dfc6534ec6047349eb7f1b806b
karanvirsb <karanvirboparai123@gmail.com>
2023-02-22 12:28:18 -0500

    removed urql and added layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,10 @@
-import { gql } from "urql";
 import { useEffect, useState } from "react";
-import client from "../urql";
-const QUERY = gql`
-	query Test {
-		getHelloWorld
-	}
-`;
+import Layout from "../shared/ui/Layout";
 
-interface testData {
-	getHelloWorld: "Hello World";
-}
 export default function Home(): JSX.Element {
-	const [data, setData] = useState("");
-
-	useEffect(() => {
-		function fetchData(): void {
-			client
-				.query(QUERY, {})
-				.toPromise()
-				.then((data) => {
-					const resp: testData = data.data;
-					setData(resp.getHelloWorld);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-
-		fetchData();
-	}, []);
-
 	return (
-		<>
-			<div className="text-red-300 text-lg">{data}</div>
-		</>
+		<Layout>
+			<></>
+		</Layout>
 	);
 }
