ab99b661f7958445e0997b2be5eae4da27fe18be
karanvirsb <karanvirboparai123@gmail.com>
2023-02-22 11:21:23 -0500

    added provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
+import { Provider } from "urql";
+import client from "../urql";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<Provider value={client}>
+			<Component {...pageProps} />;
+		</Provider>
+	);
 }
