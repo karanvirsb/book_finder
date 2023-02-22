0828dc0e285ece39451628268d1a325ab74f96e3
karanvirsb <karanvirboparai123@gmail.com>
2023-02-22 12:25:14 -0500

    created navbar

---
+++ src/shared/ui/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+	return (
+		<header>
+			<p>Book Finder</p>
+			<nav>
+				{/* TODO create page */}
+				<Link href="recently-viewed" hidden={true}>
+					Recently Viewed
+				</Link>
+				{/* TODO create search page */}
+				<Link href="books">Browse</Link>
+				{/* TODO create about page */}
+				<Link href="about">About</Link>
+				{/* TODO create login */}
+				<Link href="login">Login</Link>
+			</nav>
+		</header>
+	);
+}
