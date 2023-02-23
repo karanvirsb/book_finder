1f8a6d7e4a15bef2a3afc4faf7f3773e0db52e32
karanvirsb <karanvirboparai123@gmail.com>
2023-02-22 19:13:23 -0500

    updated with heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import Layout from "../shared/ui/Layout";
 
 export default function Home(): JSX.Element {
 	return (
-		<main className="min-h-[100svh] bg-books-background bg-center">
+		<main className="min-h-[100svh] bg-books-background bg-center bg-no-repeat">
 			<Layout>
-				<></>
+				<h1>
+					Explore the World of <span className="text-tertiary">Books</span> -
+					Find <span className="text-primary_accent">Yours</span> Now!
+				</h1>
 			</Layout>
 		</main>
 	);
