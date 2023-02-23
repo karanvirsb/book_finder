21eaaf361af7bbc125b9e9c4bcd63b526f7b2089
karanvirsb <karanvirboparai123@gmail.com>
2023-02-22 19:42:28 -0500

    centered section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import Layout from "../shared/ui/Layout";
 
 export default function Home(): JSX.Element {
 	return (
-		<main className="min-h-[100svh] bg-books-background bg-center bg-no-repeat">
+		<main className="relative min-h-[100svh] bg-books-background bg-center bg-no-repeat">
 			<Layout>
-				<section>
-					<h1 className="text-[clamp(1.69rem,calc(1.40rem+1.41vw),2.50rem)] font-semibold">
+				<section className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+					<h1 className="text-center text-[clamp(1.69rem,calc(1.40rem+1.41vw),2.50rem)] font-semibold">
 						Explore the World of <span className="text-tertiary">Books</span> -
 						Find <span className="text-primary_accent">Yours</span> Now!
 					</h1>
@@ -15,7 +15,10 @@ export default function Home(): JSX.Element {
 							className="flex-grow rounded-full border-none px-2 outline outline-1 outline-secondary"
 							placeholder="Search for Books"
 						/>
-						<button type="submit" className="uppercase">
+						<button
+							type="submit"
+							className="rounded-full bg-tertiary py-1 px-3 uppercase text-primary"
+						>
 							search
 						</button>
 					</form>
