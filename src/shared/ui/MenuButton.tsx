62a720e17c735a2992031e9aad6783e26ea4a39f
karanvirsb <karanvirboparai123@gmail.com>
2023-02-25 11:10:40 -0500

    created menu button component

---
+++ src/shared/ui/MenuButton.tsx
@@ -0,0 +1,44 @@
+interface menuButtonProps {
+	isMenuOpen: boolean;
+	setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function MenuButton({
+	isMenuOpen,
+	setIsMenuOpen,
+}: menuButtonProps): JSX.Element {
+	return (
+		<button>
+			{isMenuOpen ? (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					className="h-6 w-6"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>
+			) : (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					className="h-6 w-6"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+					/>
+				</svg>
+			)}
+		</button>
+	);
+}
