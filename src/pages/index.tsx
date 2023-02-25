5d2315df7aa8633cee4e3c295b3187a6e7fb3245
karanvirsb <karanvirboparai123@gmail.com>
2023-02-25 11:51:22 -0500

    added class into layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Searchbar from "../shared/ui/Searchbar";
 export default function Home(): JSX.Element {
 	return (
 		<main className="relative min-h-[100svh] bg-books-background bg-center bg-no-repeat">
-			<Layout>
+			<Layout classname="relative min-h-[100svh]">
 				<section className="absolute top-[50%] left-[50%] flex w-[75%] max-w-[1920px] translate-x-[-50%] translate-y-[-50%] flex-col gap-6">
 					<h1 className="text-center text-[clamp(1.69rem,calc(1.40rem+1.41vw),2.50rem)] font-semibold">
 						Explore the World of <span className="text-tertiary">Books</span> -
