4f8b0917684cabbc47e25a246f9e2ec12b0b590a
karanvirsb <karanvirboparai123@gmail.com>
2023-02-25 11:16:16 -0500

    added transition

--- src/shared/ui/MenuButton.tsx
+++ src/shared/ui/MenuButton.tsx
@@ -8,7 +8,7 @@ export default function MenuButton({
 }: menuButtonProps): JSX.Element {
 	return (
 		<button
-			className="fixed right-2 bottom-2 z-50 rounded-full bg-tertiary p-4 text-primary opacity-60 drop-shadow-md sm:hidden"
+			className="fixed right-2 bottom-2 z-50 rounded-full bg-tertiary p-4 text-primary opacity-60 drop-shadow-md transition-all duration-300 sm:hidden"
 			onClick={() => {
 				setIsMenuOpen((prev) => !prev);
 			}}
