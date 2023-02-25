60b534e04d81463f1503312e9ea2d58392c689e2
karanvirsb <karanvirboparai123@gmail.com>
2023-02-25 11:22:10 -0500

    added shared component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Layout from "../shared/ui/Layout";
+import Searchbar from "../shared/ui/Searchbar";
 
 export default function Home(): JSX.Element {
 	return (
@@ -10,18 +11,7 @@ export default function Home(): JSX.Element {
 						Explore the World of <span className="text-tertiary">Books</span> -
 						Find <span className="text-primary_accent">Yours</span> Now!
 					</h1>
-					<form className="flex flex-col gap-3 sm:flex-row">
-						<input
-							className="min-h-[35px] flex-grow rounded-full border-none px-2 outline outline-1 outline-secondary"
-							placeholder="Search for Books"
-						/>
-						<button
-							type="submit"
-							className="max-h-[35px] rounded-full bg-tertiary py-1 uppercase text-primary sm:max-h-max sm:py-2 sm:px-4"
-						>
-							search
-						</button>
-					</form>
+					<Searchbar></Searchbar>
 				</section>
 			</Layout>
 		</main>
