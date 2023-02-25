65ab2b284b354fb6310d342245ed399c93ca3683
karanvirsb <karanvirboparai123@gmail.com>
2023-02-25 11:10:52 -0500

    added setter

--- src/shared/ui/MobileNavbar.tsx
+++ src/shared/ui/MobileNavbar.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import React, { useState } from "react";
+import MenuButton from "./MenuButton";
 
 export default function MobileNavbar(): JSX.Element {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	return (
 		<>
 			{/* menu button */}
-			{<MenuButton isMenuOpen={isMenuOpen}></MenuButton>}
+			{
+				<MenuButton
+					isMenuOpen={isMenuOpen}
+					setIsMenuOpen={setIsMenuOpen}
+				></MenuButton>
+			}
 			{isMenuOpen ? (
 				<header className="fixed bottom-0 left-0 flex w-full flex-col items-center justify-between bg-primary pt-4 sm:hidden">
 					<p className="text-xl font-semibold">Book Finder</p>
@@ -38,45 +44,5 @@ export default function MobileNavbar(): JSX.Element {
 				</header>
 			) : null}
 		</>
-	);
-}
-interface menuButtonProps {
-	isMenuOpen: boolean;
-}
-function MenuButton({ isMenuOpen }: menuButtonProps): JSX.Element {
-	return (
-		<button>
-			{isMenuOpen ? (
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={1.5}
-					stroke="currentColor"
-					className="h-6 w-6"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M6 18L18 6M6 6l12 12"
-					/>
-				</svg>
-			) : (
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={1.5}
-					stroke="currentColor"
-					className="h-6 w-6"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-					/>
-				</svg>
-			)}
-		</button>
 	);
 }
