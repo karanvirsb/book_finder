ba31acbd1fd30207693f30404f27310b2947d15e
karanvirsb <karanvirboparai123@gmail.com>
2023-02-25 11:50:34 -0500

    added ,aom amd section to layout

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Layout from "../../shared/ui/Layout";
+import Searchbar from "../../shared/ui/Searchbar";
 
 export default function Books(): JSX.Element {
 	return (
-		<main className="relative min-h-[100svh]">
-			<Layout></Layout>
-		</main>
+		<>
+			<main className="max-h-max bg-books-background">
+				<Layout>
+					<section className="my-10 w-full  py-10">
+						<Searchbar></Searchbar>
+					</section>
+				</Layout>
+			</main>
+		</>
 	);
 }
