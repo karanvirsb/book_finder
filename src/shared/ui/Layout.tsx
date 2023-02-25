cce74b43f86ff6d03c4ccdb4218d6aed839e6263
karanvirsb <karanvirboparai123@gmail.com>
2023-02-25 11:43:28 -0500

    passed prop

--- src/shared/ui/Layout.tsx
+++ src/shared/ui/Layout.tsx
@@ -4,12 +4,16 @@ import MobileNavbar from "./MobileNavbar";
 
 interface props {
 	children?: React.ReactNode;
+	navbarClassName?: string;
 }
 
-export default function Layout({ children }: props): JSX.Element {
+export default function Layout({
+	children,
+	navbarClassName,
+}: props): JSX.Element {
 	return (
 		<main className="relative min-h-[100svh] px-[clamp(2rem,1rem+7vw,7rem)]">
-			<Navbar></Navbar>
+			<Navbar classname={navbarClassName}></Navbar>
 			<MobileNavbar></MobileNavbar>
 			{children}
 		</main>
