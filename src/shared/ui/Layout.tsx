2ded8eb1c19679fc31349a708be0d55a6b9c8fd2
karanvirsb <karanvirboparai123@gmail.com>
2023-02-25 10:52:19 -0500

    added mobile nav bar

--- src/shared/ui/Layout.tsx
+++ src/shared/ui/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import MobileNavbar from "./MobileNavbar";
 
 interface props {
 	children: React.ReactNode;
@@ -9,6 +10,7 @@ export default function Layout({ children }: props): JSX.Element {
 	return (
 		<main className="relative min-h-[100svh] px-[clamp(2rem,1rem+7vw,7rem)]">
 			<Navbar></Navbar>
+			<MobileNavbar></MobileNavbar>
 			{children}
 		</main>
 	);
