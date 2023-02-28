83a6d8f06bad72326df038683148d2f933c9170a
karanvirsb <karanvirboparai123@gmail.com>
2023-02-28 11:59:14 -0500

    added submit functionality and search query

--- src/shared/ui/Searchbar.tsx
+++ src/shared/ui/Searchbar.tsx
@@ -16,10 +16,17 @@ export default function Searchbar({
 			<input
 				className="min-h-[35px] flex-grow rounded-full border-none px-2 outline outline-1 outline-secondary"
 				placeholder="Search for Books"
+				value={searchQuery}
+				onChange={(e) => {
+					setSearchQuery(e.target.value);
+				}}
 			/>
 			<button
 				type="submit"
 				className="max-h-[35px] rounded-full bg-tertiary py-1 uppercase text-primary sm:max-h-max sm:py-2 sm:px-4"
+				onClick={() => {
+					submitCb();
+				}}
 			>
 				search
 			</button>
