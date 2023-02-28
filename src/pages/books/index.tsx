5e242edb7baf1425eab6170955a8fa118e133bbc
karanvirsb <karanvirboparai123@gmail.com>
2023-02-28 10:19:05 -0500

    added query and book map

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Layout from "../../shared/ui/Layout";
 import Searchbar from "../../shared/ui/Searchbar";
+import client from "../../urql";
+import {
+	GetBooksByTitleDocument,
+	useGetBooksByTitleQuery,
+} from "../../generated/graphql";
+import { useQuery } from "urql";
+import Book from "./components/Book";
 
 export default function Books(): JSX.Element {
+	const [{ data, fetching, error }] = useGetBooksByTitleQuery({
+		variables: { limit: 10, page: 0, searchQuery: "" },
+	});
 	return (
 		<>
 			<main className="max-h-max bg-books-background">
@@ -12,7 +22,25 @@ export default function Books(): JSX.Element {
 					</section>
 				</Layout>
 			</main>
-			<section>{/* Books */}</section>
+			<section>
+				{/* // add spinner */}
+				{fetching ? <p>Loading...</p> : null}
+				{error != null ? <p>Oh no ... {error.message}</p> : null}
+				{data?.getBooksByTitleResolver != null
+					? data.getBooksByTitleResolver.map((book) => {
+							return (
+								<Book
+									asin={book?.asin ?? ""}
+									author={book?.author?.name ?? ""}
+									description={book?.description ?? ""}
+									imageStr={book?.image_url ?? ""}
+									title={book?.title ?? ""}
+									key={book?.asin}
+								></Book>
+							);
+					  })
+					: "Could not find any books."}
+			</section>
 		</>
 	);
 }
