938007a72bee7aed7956a1a7a89cc5eb45757bdf
karanvirsb <karanvirboparai123@gmail.com>
2023-02-28 10:13:59 -0500

    added provider and client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import "../styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from "next/app";
+import { Provider } from "urql";
+import client from "../urql";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<main className={inter.className}>
-			<Component {...pageProps} />
-		</main>
+		<Provider value={client}>
+			<main className={inter.className}>
+				<Component {...pageProps} />
+			</main>
+		</Provider>
 	);
 }
