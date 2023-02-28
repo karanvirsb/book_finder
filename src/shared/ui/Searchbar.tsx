ebd1ac9fb3cc2059db78efa04bcbae05dfd5ff9f
karanvirsb <karanvirboparai123@gmail.com>
2023-02-28 12:05:43 -0500

    updated types and added submit

--- src/shared/ui/Searchbar.tsx
+++ src/shared/ui/Searchbar.tsx
@@ -3,7 +3,7 @@ import React, { type SetStateAction } from "react";
 interface ISearchbar {
 	setSearchQuery: React.Dispatch<SetStateAction<string>>;
 	searchQuery: string;
-	submitCb: () => void;
+	submitCb: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
 export default function Searchbar({
@@ -12,7 +12,12 @@ export default function Searchbar({
 	submitCb,
 }: ISearchbar): JSX.Element {
 	return (
-		<form className="flex flex-col gap-3 sm:flex-row">
+		<form
+			className="flex flex-col gap-3 sm:flex-row"
+			onSubmit={(e) => {
+				submitCb(e);
+			}}
+		>
 			<input
 				className="min-h-[35px] flex-grow rounded-full border-none px-2 outline outline-1 outline-secondary"
 				placeholder="Search for Books"
@@ -24,9 +29,6 @@ export default function Searchbar({
 			<button
 				type="submit"
 				className="max-h-[35px] rounded-full bg-tertiary py-1 uppercase text-primary sm:max-h-max sm:py-2 sm:px-4"
-				onClick={() => {
-					submitCb();
-				}}
 			>
 				search
 			</button>
