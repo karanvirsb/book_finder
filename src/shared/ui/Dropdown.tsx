b13b9f674cc2a0139e199cbf7ef08577bf35f646
karanvirsb <karanvirboparai123@gmail.com>
2023-03-02 09:33:08 -0500

    added use state for opening and closing

--- src/shared/ui/Dropdown.tsx
+++ src/shared/ui/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface dropdownProps {
 	name?: string;
@@ -6,6 +6,7 @@ interface dropdownProps {
 }
 
 export default function Dropdown({ name, children }: dropdownProps) {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<div className="relative inline-block text-left">
 			<div>
@@ -15,6 +16,9 @@ export default function Dropdown({ name, children }: dropdownProps) {
 					id="menu-button"
 					aria-expanded="true"
 					aria-haspopup="true"
+					onClick={() => {
+						setIsOpen((prev) => !prev);
+					}}
 				>
 					{name ?? "Options"}
 					<svg
@@ -31,55 +35,28 @@ export default function Dropdown({ name, children }: dropdownProps) {
 					</svg>
 				</button>
 			</div>
-			<div
-				className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-				role="menu"
-				aria-orientation="vertical"
-				aria-labelledby="menu-button"
-				tabIndex={-1}
-			>
-				<div className="py-1" role="none">
-					{children}
-					<a
-						href="#"
-						className="block px-4 py-2 text-sm text-gray-700"
-						role="menuitem"
-						tabIndex={-1}
-						id="menu-item-0"
-					>
-						Account settings
-					</a>
-					<a
-						href="#"
-						className="block px-4 py-2 text-sm text-gray-700"
-						role="menuitem"
-						tabIndex={-1}
-						id="menu-item-1"
-					>
-						Support
-					</a>
-					<a
-						href="#"
-						className="block px-4 py-2 text-sm text-gray-700"
-						role="menuitem"
-						tabIndex={-1}
-						id="menu-item-2"
-					>
-						License
-					</a>
-					<form method="POST" action="#" role="none">
-						<button
-							type="submit"
-							className="block w-full px-4 py-2 text-left text-sm text-gray-700"
+			{isOpen ? (
+				<div
+					className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+					role="menu"
+					aria-orientation="vertical"
+					aria-labelledby="menu-button"
+					tabIndex={-1}
+				>
+					<div className="py-1" role="none">
+						{children}
+						<a
+							href="#"
+							className="block px-4 py-2 text-sm text-gray-700"
 							role="menuitem"
 							tabIndex={-1}
-							id="menu-item-3"
+							id="menu-item-0"
 						>
-							Sign out
-						</button>
-					</form>
+							Test
+						</a>
+					</div>
 				</div>
-			</div>
+			) : null}
 		</div>
 	);
 }
