32e665aa0849aad0a5d741d9d4dc5cb0b4b5f7a0
karanvirsb <karanvirboparai123@gmail.com>
2023-03-02 09:56:21 -0500

    added change event handler

--- src/shared/ui/SelectDropdown.tsx
+++ src/shared/ui/SelectDropdown.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { type ChangeEventHandler } from "react";
 
 interface dropdownProps {
 	name: string;
 	children?: React.ReactNode;
+	onchange: ChangeEventHandler;
 }
 
-export default function SelectDropdown({ name, children }: dropdownProps) {
+export default function SelectDropdown({
+	name,
+	children,
+	onchange,
+}: dropdownProps): JSX.Element {
 	return (
 		<>
 			<label htmlFor={`dropdown-${name}`}>Per Page:</label>
 			<select
 				className="relative ml-4 inline-block rounded-md bg-white px-3 py-2 text-left text-sm font-semibold text-secondary hover:bg-gray-50 hover:text-primary_accent"
 				id={`dropdown-${name}`}
+				onChange={onchange}
 			>
 				{children}
 			</select>
