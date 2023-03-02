369ca92b6eb2768bc266da01930dac22c5071801
karanvirsb <karanvirboparai123@gmail.com>
2023-03-02 09:53:33 -0500

    changed text color on hover

--- src/shared/ui/SelectDropdown.tsx
+++ src/shared/ui/SelectDropdown.tsx
@@ -10,7 +10,7 @@ export default function SelectDropdown({ name, children }: dropdownProps) {
 		<>
 			<label htmlFor={`dropdown-${name}`}>Per Page:</label>
 			<select
-				className="relative ml-4 inline-block rounded-md bg-white px-3 py-2 text-left text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+				className="relative ml-4 inline-block rounded-md bg-white px-3 py-2 text-left text-sm font-semibold text-secondary hover:bg-gray-50 hover:text-primary_accent"
 				id={`dropdown-${name}`}
 			>
 				{children}
