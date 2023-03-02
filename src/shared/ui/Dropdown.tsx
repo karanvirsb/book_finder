c5b40f416813407d0977f442d4c1a11c91b92de3
karanvirsb <karanvirboparai123@gmail.com>
2023-03-02 09:37:09 -0500

    changed width

--- src/shared/ui/Dropdown.tsx
+++ src/shared/ui/Dropdown.tsx
@@ -37,7 +37,7 @@ export default function Dropdown({ name, children }: dropdownProps) {
 			</div>
 			{isOpen ? (
 				<div
-					className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+					className="absolute right-0 z-10 mt-2 w-max origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 					role="menu"
 					aria-orientation="vertical"
 					aria-labelledby="menu-button"
@@ -45,15 +45,6 @@ export default function Dropdown({ name, children }: dropdownProps) {
 				>
 					<div className="py-1" role="none">
 						{children}
-						<a
-							href="#"
-							className="block px-4 py-2 text-sm text-gray-700"
-							role="menuitem"
-							tabIndex={-1}
-							id="menu-item-0"
-						>
-							Test
-						</a>
 					</div>
 				</div>
 			) : null}
