17bfdae9cb33de5fdff74e3c42df0c267d701681
karanvirsb <karanvirboparai123@gmail.com>
2023-03-09 10:21:32 -0500

    created state management

---
+++ src/shared/zustand/bookParams.ts
@@ -0,0 +1,21 @@
+"use client";
+import { useDebugValue } from "react";
+import { create } from "zustand";
+
+export interface paramsType {
+	searchQuery: string;
+	limit: number;
+	page: number;
+}
+
+interface BookParamsState {
+	params: paramsType;
+	setParams: (params: Partial<paramsType>) => void;
+}
+
+export const useBookParamsStore = create<BookParamsState>()((set) => ({
+	params: { searchQuery: "", limit: 10, page: 0 },
+	setParams: (newParams) => {
+		set((state) => ({ params: { ...state.params, ...newParams } }));
+	},
+}));
