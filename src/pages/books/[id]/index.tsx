45b94575f2d53638f475a488e9f18559a366fe2c
karanvirsb <karanvirboparai123@gmail.com>
2023-03-09 11:30:00 -0500

    added urql serverside rendering

--- src/pages/books/[id]/index.tsx
+++ src/pages/books/[id]/index.tsx
@@ -1,5 +1,37 @@
+import { type GetServerSidePropsContext } from "next";
+import { withUrqlClient, initUrqlClient, type SSRData } from "next-urql";
 import React from "react";
+import { cacheExchange, dedupExchange, fetchExchange, ssrExchange } from "urql";
 
-export default function BookDetails(): JSX.Element {
+function BookDetails(): JSX.Element {
 	return <div>BookDetails</div>;
 }
+
+export async function getServerSideProps(
+	ctx: GetServerSidePropsContext
+): Promise<{
+	props: {
+		urqlState: SSRData;
+	};
+}> {
+	const ssrCache = ssrExchange({ isClient: false });
+	const client = initUrqlClient(
+		{
+			url: "http://localhost:3000/api/graphql", // not needed without `fetchExchange`
+			exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
+		},
+		false
+	);
+
+	await client?.query(TODOS_QUERY).toPromise();
+
+	return {
+		props: {
+			urqlState: ssrCache.extractData(),
+		},
+	};
+}
+
+export default withUrqlClient((ssr) => ({
+	url: "your-url",
+}))(BookDetails);
