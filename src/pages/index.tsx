e9c71e594ef5b7e71e45d6aea012aa74c6aaad3f
karanvirsb <karanvirboparai123@gmail.com>
2023-03-20 14:04:49 -0400

    replaced

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,9 @@ export default function Home(): JSX.Element {
 			<Layout classname="relative min-h-[100svh]">
 				<section className="absolute top-[50%] left-[50%] flex w-[75%] max-w-[1920px] translate-x-[-50%] translate-y-[-50%] flex-col gap-6">
 					<h1 className="text-center text-[clamp(1.69rem,calc(1.40rem+1.41vw),2.50rem)] font-semibold">
-						Explore the World of <span className="text-tertiary">Books</span> -
-						Find <span className="text-primary_accent">Yours</span> Now!
+						Explore the World of{" "}
+						<span className="text-tertiary-500">Books</span> - Find{" "}
+						<span className="text-primary_accent">Yours</span> Now!
 					</h1>
 					<Searchbar
 						searchQuery={searchQueryRef}
