59e212688c66475e6a3cac912c9e9b476cdf2195
karanvirsb <karanvirboparai123@gmail.com>
2023-03-22 21:29:57 -0400

    added session provider for auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { Provider } from "urql";
 import client from "../urql";
 
@@ -8,10 +9,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Provider value={client}>
-			<main className={inter.className}>
-				<Component {...pageProps} />
-			</main>
-		</Provider>
+		<SessionProvider session={pageProps.session}>
+			<Provider value={client}>
+				<main className={inter.className}>
+					<Component {...pageProps} />
+				</main>
+			</Provider>
+		</SessionProvider>
 	);
 }
