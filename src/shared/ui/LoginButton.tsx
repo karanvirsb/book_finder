5cd15768a7dc8c74a65fe3eb18e41828aae9d25f
karanvirsb <karanvirboparai123@gmail.com>
2023-03-22 21:42:08 -0400

    created button for login and log out

---
+++ src/shared/ui/LoginButton.tsx
@@ -0,0 +1,28 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import React from "react";
+
+export default function LoginButton(): JSX.Element {
+	const { data: session } = useSession();
+	if (session != null) {
+		return (
+			<button
+				className="hover:text-secondary hover:outline-secondary rounded-full border-none bg-secondary-500 py-1 px-4 text-primary-500 hover:bg-transparent hover:font-bold hover:outline"
+				onClick={() => {
+					void signOut();
+				}}
+			>
+				Signout
+			</button>
+		);
+	}
+	return (
+		<button
+			className="hover:text-secondary hover:outline-secondary rounded-full border-none bg-secondary-500 py-1 px-4 text-primary-500 hover:bg-transparent hover:font-bold hover:outline"
+			onClick={() => {
+				void signIn();
+			}}
+		>
+			Login
+		</button>
+	);
+}
